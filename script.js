e24aff9a67e37c4a635172b0b5d3fbdb234d7d2f
Anna Panferova <anyapanf@gmail.com>
2023-08-03 20:26:06 +0600

    final version added

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ let hor = document.querySelector('#horizontal')
 let blur = document.querySelector('#blur')
 let spread = document.querySelector('#spread')
 let inp = Array.from(document.querySelectorAll('input'))
-let text = document.createElement('p')
-box.appendChild(text)
-text.className = 'text'
+let bcg__color = document.querySelector('#bcg__color')
+let box__color = document.querySelector('#box__color')
+let generator = document.querySelector('.generator')
+let code = document.querySelector('.generator__code')
+let radius = document.querySelector('#radius')
 
 inp.forEach((el) => {
     el.addEventListener('input', () => {
         box.style.boxShadow = `${horizontal.value}px ${vert.value}px ${blur.value}px ${spread.value}px ${color.value}`
-        text.innerText = `box-shadow: ${horizontal.value}px ${vert.value}px ${blur.value}px ${spread.value}px ${color.value}`
+        generator.style.backgroundColor = `${bcg__color.value}`
+        box.style.backgroundColor = `${box__color.value}`
+        code.innerText = `box-shadow: ${horizontal.value}px ${vert.value}px ${blur.value}px ${spread.value}px ${color.value}`
+        box.style.borderBottomLeftRadius = `${radius.value}px`
+        box.style.borderBottomRightRadius = `${radius.value}px`
+        box.style.borderTopLeftRadius = `${radius.value}px`
+        box.style.borderTopRightRadius = `${radius.value}px`
+
     })
 })
 console.dir(box);
